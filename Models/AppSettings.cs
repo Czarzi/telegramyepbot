0d29e7bd7bf2171ea30940edeeddf59f4d444740
Czarzi <47085791+Czarzi@users.noreply.github.com>
2019-07-13 00:11:05 +0600

    Add files via upload

---
+++ Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramYepBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; }  = "https://telegramyepbot.azurewebsites.net:443/{0}";
+
+        public static string Name { get; set; } = "extremecode_bot";
+
+        public static string Key { get; set; }  = "657247879:AAHQrTnCVVOFS5C48oOUfeQvvnd8nov3isw";
+
+    }
+}
